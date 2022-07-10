e6d17b553f18237e586ae35bbef053d6cea6d253
YoungCheon Kim <kyt031522@gmail.com>
2022-07-10 12:12:25 +0900

    js

--- CloneCoding/app.js
+++ CloneCoding/app.js
@@ -1,4 +1,33 @@
 let a = 1;
+const age = prompt("HI");
+console.log(age);
+
+
+
+
+
+
+
+
+
+
+const title = document.getElementsByClassName("flex");
+title[0].autofocus = true;
+console.dir(title);          //js로 HTML 변경 가능    해당 클래스를 가진 태그를 가져온다.
+
+const flex = document.querySelector(".flex div");  //하나만 가져옴  하위 태그를 가져올 수 있다.
+console.dir(flex);
+
+const flex1 = document.querySelectorAll(".flex div");  //모두 가져옴  가져온 태그 내에 있는 style 객체도 변경 가능
+console.dir(flex1);
+
+
+
+
+
+
+
+
 
 function sayHello(){
     console.log("Hello")
@@ -10,3 +39,33 @@ player = {
     }
 }
 player.sayHello();
+
+
+
+
+
+function changeclassname(){
+    h1.className = "flex";     //클래스 변경 가능 
+    h1.classList.contains(flex);   //클래스에 flex클래스가 있는지 알려줌
+    h1.classList.add(flex);       //클래스에서 flex클래스 추가
+    h1.classList.remove(flex);     //클래스에서 flex클래스 제거
+    h1.classList.toggle(flex);     //클래스에 flex가 없으면 추가하고, 있으면 제거
+}
+
+
+function clickevent(){
+    console.log("hi");
+}
+
+function mousehere(){
+    console.log("1111");
+}
+
+function resizewindow(){
+    document.body.style.backgroundColor = "gray";
+}
+
+flex.addEventListener("click",clickevent);   //클릭이란 이벤트를 추가, 함수 실행
+flex.onclick = clickevent;  //위와 같음
+flex.addEventListener("mouseenter", mousehere);
+window.addEventListener("resize",resizewindow);
