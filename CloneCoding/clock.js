7f10b34d47d3397e5f77989ef77eed278191c204
YoungCheon Kim <kyt031522@gmail.com>
2022-07-15 11:09:07 +0900

    clock,quote

--- CloneCoding/clock.js
+++ CloneCoding/clock.js
@@ -1,12 +1,17 @@
 const clock = document.querySelector("#clock");
-let time = 1
-clock.innerText = time;
+getDATA();
 
-
-function sayHello(){
-    clock.innerText = time;
-    time +=1;
+function getDATA(){
+    const date = new Date();
+    
+    const hour = String(date.getHours());
+    const minute = String(date.getMinutes());
+    const second = String(date.getSeconds());
+    clock.innerText = `${hour.padStart(2,0)} : ${minute.padStart(2,0)} : ${second.padStart(2,0)}`;
 }
 
+
+
+setInterval(getDATA,1000);
 //setInterval(sayHello,1000); //1000ms 간격으로 함수 실행
 //setTimeout(sayHello,5000);  // 5000ms 딜레이 후 함수 실행
